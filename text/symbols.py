9b108aeda0df9301519c021260d72df0abfc1802
Lina <43246784+zhanglina94@users.noreply.github.com>
2023-11-30 15:41:00 +0900

    Create symbols.py

---
+++ text/symbols.py
@@ -0,0 +1,17 @@
+'''
+Defines the set of symbols used in text input to the model.
+'''
+
+# Symbols for en/ko/ja cleaners - from MB-iSTFT-VITS-multilingual
+_pad        = '_'
+_punctuation = ';:,.!?¡¿—…"«»“” '
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅓㅗㅜㅡㅣㅐㅔ'
+_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+#'''
+
+# Export all symbols:
+#symbols = [_pad] + list(_punctuation) + list(_letters)
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) # !
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
